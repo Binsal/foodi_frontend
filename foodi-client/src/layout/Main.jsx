1982337b073b579d39685a1273192018ce92e5a6
Saloni Bindal <salonibindal81@gmail.com>
2024-01-19 13:44:56 +0530

    navbar added

--- foodi-client/src/layout/Main.jsx
+++ foodi-client/src/layout/Main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Main = ()=>{
     return(
         <>
-            <nav>Navbar</nav>
+            <Navbar/>
             <Outlet/>
             <footer>Footer</footer>
         </>
