95ab9585c42ea7cc2c66313fddcff9908aea4f35
Saloni Bindal <salonibindal81@gmail.com>
2024-01-20 10:12:50 +0530

    added favourites dishes slider

--- foodi-client/src/pages/home/Home.jsx
+++ foodi-client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "../../components/Banner"
 import Catagories from "./Categories";
+import SpecialDishes from "./SpecialDishes";
 const Home = ()=>{
     return (
         <div>
            <Banner/>
            <Catagories/>
+           <SpecialDishes/>
         </div>
     )
 }
