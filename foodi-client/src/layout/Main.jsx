1d30fc4f9eea76599a5d7fe82929f2abf1fbed05
Saloni Bindal <salonibindal81@gmail.com>
2024-01-20 11:06:52 +0530

    added footer

--- foodi-client/src/layout/Main.jsx
+++ foodi-client/src/layout/Main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Main = ()=>{
     return(
         <>
             {/* <Navbar/> */}
             <Outlet/>
-            <footer>Footer</footer>
+            <Footer/>
         </>
     )
 }
