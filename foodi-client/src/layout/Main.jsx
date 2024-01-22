d7155ee37d4369402948a5aacbb3225820daea59
Saloni Bindal <salonibindal81@gmail.com>
2024-01-22 15:00:09 +0530

    menu page completed with filters

--- foodi-client/src/layout/Main.jsx
+++ foodi-client/src/layout/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import "../../src/App.css";
 
 const Main = ()=>{
     return(
