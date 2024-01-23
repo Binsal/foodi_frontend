48c899931a9373fff2acc400eeb417199ad0b2f2
Saloni Bindal <salonibindal81@gmail.com>
2024-01-23 18:53:53 +0530

    completed authProvider code

--- foodi-client/src/components/Navbar.jsx
+++ foodi-client/src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import React, { useContext, useEffect, useState } from "react";
 import logo from "/logo.png";
 import { FaRegUser } from "react-icons/fa";
 import Modal from "./Modal";
+import { AuthContext } from "../contexts/AuthProvider";
 
 const Navbar = () => {
   const [isSticky, setSticky] = useState(false);
 
-  // const {user} = useContext(AuthContext);
-  // console.log(user)
+  const {user} = useContext(AuthContext);
+  console.log(user)
 
   useEffect(() => {
     const handleScroll = () => {
--- foodi-client/src/main.jsx
+++ foodi-client/src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import router from './router/Router'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-
+import AuthProvider from './contexts/AuthProvider'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <AuthProvider>
+     <RouterProvider router={router} />
+  </AuthProvider>,
 )
